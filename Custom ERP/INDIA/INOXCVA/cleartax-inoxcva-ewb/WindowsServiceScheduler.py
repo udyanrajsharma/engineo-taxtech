0f2f68ba3bf1a3f83ccb14d9baecdfce8d809c77
Udyan Raj Sharma <udyanrajsharma@gmail.com>
2024-05-08 01:13:19 +0530

    Working as Windows Service

--- Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-ewb/WindowsServiceScheduler.py
+++ Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-ewb/WindowsServiceScheduler.py
@@ -1,34 +1,90 @@
+from multiprocessing import Process
+import os
+import sys
+import traceback
 import schedule
-from application.ClearTaxEWBwithoutIRN import ClearTaxEWBwithoutIRN
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+
+# from application.ClearTaxEWBwithoutIRN import ClearTaxEWBwithoutIRN
 import time
-from pathlib import Path
-from infrastructure.pythonWindowsService import PythonWindowsService
+import logging
+import logging.handlers
+
+# Sets log file path.
+log_file = "d:\\output.log"
+
+# Return a logger with the specified name.
+servicelogger = logging.getLogger("ClearTaxEWBServiceLogger")
+
+# Sets the threshold for this logger to lvl. Logging messages which are less severe than lvl will be ignored.
+servicelogger.setLevel(logging.DEBUG)
+
+handler = logging.handlers.RotatingFileHandler(
+    log_file, maxBytes=10485760, backupCount=10
+)
+
+# Sets format of record in log file
+formatter = logging.Formatter(
+    "%(asctime)s - %(module)-10s - %(levelname)-8s %(message)s", "%d-%m-%Y %H:%M:%S"
+)
+handler.setFormatter(formatter)
+
+# Adds the specified handler to logger "MyLogger"
+servicelogger.addHandler(handler)
 
 
-class PythonCornerExample(PythonWindowsService):
-    _svc_name_ = "ClearTax-EWB-WithoutIRN"
-    _svc_display_name_ = "ClearTax - EWB without IRN"
-    _svc_description_ = "ClearTax - EWB without IRN"
+class WindowsServiceScheduler(win32serviceutil.ServiceFramework):
+    _svc_name_ = "INOX ClearTax EWB Without IRN"
+    _svc_display_name_ = "INOX ClearTax EWB Without IRN"
 
-    def start(self):
-        self.isrunning = True
+    def __init__(self, *args):
+        super().__init__(*args)
 
-    def stop(self):
-        self.isrunning = False
+    # stop command service
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        self.process.terminate()
+        self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
+    # start command service
+    def SvcDoRun(self):
+        servicelogger.info("*** STARTING SERVICE ***\n")
+        try:  # try main
+            self.main()
+        except:
+            servicemanager.LogErrorMsg(
+                traceback.format_exc()
+            )  # if error print it to event log
+            os._exit(-1)
+
+    # main process
     def main(self):
-        while self.isrunning:
-            try:
-                schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.EWBwithoutIRN)
-                schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.updateEWB)
-                schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.cancelEWB)
-            except:
-                print("Scheduler is not called")
 
-            while True:
-                schedule.run_pending()
-                time.sleep(1)
+        servicelogger.info("... STARTING SCHEDULE PROCESS ...\n")
+        schedule.every(10).seconds.do(self.scheduler)
+
+        # schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.EWBwithoutIRN)
+        # schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.updateEWB)
+        # schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.cancelEWB)
+        # run main loop for schedule process while service runs
+        while True:
+            # execute task on schedule
+            schedule.run_pending()
+            time.sleep(1)
+
+    def scheduler(self):
+        servicelogger.info("... Scheduler active ...\n")
 
 
 if __name__ == "__main__":
-    PythonCornerExample.parse_command_line()
+    if len(sys.argv) > 1:
+        # Called by Windows shell. Handling arguments such as: Install, Remove, etc.
+        win32serviceutil.HandleCommandLine(WindowsServiceScheduler)
+    else:
+        # Called by Windows Service. Initialize the service to communicate with the system operator
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(WindowsServiceScheduler)
+        servicemanager.StartServiceCtrlDispatcher()
