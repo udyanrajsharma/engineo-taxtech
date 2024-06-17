1319c48ad885fd3e637e8f14fa15c50b2e467f86
Acharya-Animesh <115926066+Acharya-Animesh@users.noreply.github.com>
2024-06-17 10:57:04 +0530

    INOX working code

--- Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-non-irn-ewb/WindowsServiceScheduler.py
+++ Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-non-irn-ewb/WindowsServiceScheduler.py
@@ -13,35 +13,11 @@ import time
 import logging
 import logging.handlers
 
-from dotenv import load_dotenv
-import os
-import configparser
-import base64
-import sys
 
-config = configparser.ConfigParser()
-extDataDir = os.getcwd()
-config_path = extDataDir+'/property2.ini'
-config.read(config_path)
-
-def decode_value(encoded_str):
-    decoded_bytes = base64.b64decode(encoded_str.encode('utf-8'))
-    decoded_str = decoded_bytes.decode('utf-8')
-    return decoded_str
-
-logFilePath = decode_value(config.get('WINDOWS_SERVICE', 'log_filepath'))
-service_name = decode_value(config.get('WINDOWS_SERVICE', 'servicename'))
-
-extDataDir = os.getcwd()
-if getattr(sys, "frozen", False):
-    extDataDir = sys._MEIPASS
-load_dotenv(dotenv_path=os.path.join(extDataDir, ".env"))
-
-# logFilePath = os.getenv("log_filePath")
-# service_name = os.getenv("serviceName")
+serviceName = "INOX ClearTax EWB Without IRN V2"
 
 # Sets log file path.
-log_file = logFilePath
+log_file_info = "D:\\output.log"
 
 # Return a logger with the specified name.
 servicelogger = logging.getLogger("ClearTaxEWBServiceLogger")
@@ -50,7 +26,7 @@ servicelogger = logging.getLogger("ClearTaxEWBServiceLogger")
 servicelogger.setLevel(logging.DEBUG)
 
 handler = logging.handlers.RotatingFileHandler(
-    log_file, maxBytes=10485760, backupCount=10
+    log_file_info, maxBytes=10485760, backupCount=10
 )
 
 # Sets format of record in log file
@@ -62,10 +38,9 @@ handler.setFormatter(formatter)
 # Adds the specified handler to logger "MyLogger"
 servicelogger.addHandler(handler)
 
-
 class WindowsServiceScheduler(win32serviceutil.ServiceFramework):
-    _svc_name_ = service_name
-    _svc_display_name_ = service_name
+    _svc_name_ = serviceName
+    _svc_display_name_ = serviceName
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -74,12 +49,15 @@ class WindowsServiceScheduler(win32serviceutil.ServiceFramework):
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         self.process.terminate()
+        self.isrunning = False
+        win32event.SetEvent(self.hWaitStop)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
     # start command service
     def SvcDoRun(self):
         servicelogger.info("*** STARTING SERVICE ***\n")
         try:  # try main
+            self.isrunning = True
             self.main()
         except:
             servicemanager.LogErrorMsg(
@@ -89,18 +67,21 @@ class WindowsServiceScheduler(win32serviceutil.ServiceFramework):
 
     # main process
     def main(self):
+        try:
+            servicelogger.info("... STARTING SCHEDULE PROCESS ...\n")
+            schedule.every(10).seconds.do(self.scheduler)
+            schedule.every(15).seconds.do(ClearTaxEWBwithoutIRN.testTableModel)
+            # schedule.every(2).minutes.do(ClearTaxEWBwithoutIRN.EWBwithoutIRN)
+            # schedule.every(2).minutes.do(ClearTaxEWBwithoutIRN.updateEWB)
+            # schedule.every(2).minutes.do(ClearTaxEWBwithoutIRN.cancelEWB)
+            # run main loop for schedule process while service runs
+            while self.isrunning:
+                # execute task on schedule
+                schedule.run_pending()
+                time.sleep(1)
+        except Exception as e:
+            print("An error occurred:", e)
 
-        servicelogger.info("... STARTING SCHEDULE PROCESS ...\n")
-        schedule.every(10).seconds.do(self.scheduler)
-
-        schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.EWBwithoutIRN)
-        schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.updateEWB)
-        schedule.every(5).minutes.do(ClearTaxEWBwithoutIRN.cancelEWB)
-        # run main loop for schedule process while service runs
-        while True:
-            # execute task on schedule
-            schedule.run_pending()
-            time.sleep(1)
 
     def scheduler(self):
         servicelogger.info("... Scheduler active ...\n")
