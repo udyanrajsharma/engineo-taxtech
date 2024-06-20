c35ea52eb03a8cce9d54f9531dfb7c534d00b5ab
Acharya-Animesh <115926066+Acharya-Animesh@users.noreply.github.com>
2024-06-20 22:02:31 +0530

    INOX : Updated Database connection open and close

--- Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-non-irn-ewb-prod-server/infrastructure/database.py
+++ Custom ERP/INDIA/INOXCVA/cleartax-inoxcva-non-irn-ewb-prod-server/infrastructure/database.py
@@ -37,43 +37,65 @@ DB_PASSWORD = decode_value(config.get('DB_CONNECTION', 'DB_PASSWORD'))
 DB_SERVER = decode_value(config.get('DB_CONNECTION', 'DB_SERVER'))
 
 # Connect to MSSQL
-connection = pymssql.connect(
-    server = DB_SERVER,
-    user= DB_USER,
-    password= DB_PASSWORD,
-    database= DB_NAME
-)
-print("database Connected")
+# connection = pymssql.connect(
+#     server = DB_SERVER,
+#     user= DB_USER,
+#     password= DB_PASSWORD,
+#     database= DB_NAME
+# )
 
 class database:
 
+    def databaseConnection():
+        try:
+            connection = pymssql.connect(
+                server = DB_SERVER,
+                user= DB_USER,
+                password= DB_PASSWORD,
+                database= DB_NAME
+            )
+            servicelogger_info.info("database connected")
+            return connection
+        except Exception as e:
+            servicelogger_error.exception("..Exception Occured during Connection with database...\n")
+
     def executeHeaderDBQuery():
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             Header_query = "select distinct [Document Number], CONVERT(varchar(10), [Document Date],103) [Document Date],  case when [Document Type] = 'Invoice' then 'INV' when [Document Type] = 'Delivery Challan' then 'CHL' when [Document Type] = 'Other' then 'Oth' else [Document Type] end [Document Type], [Transaction Type], case when [Sub Type] = 'jobwork' then 'JOB_WORK' when [Sub Type] = 'supply' then 'Supply' when [Sub Type] = 'Own use' then 'OWN_USE' when [Sub Type] = 'Export' then 'Export' when [Sub Type] = 'SKD' then 'SKD_CKD_LOTS' when [Sub Type] = 'Others' then 'OTH' when [Sub Type] = 'Other' then 'OTH' else [Sub Type] end [Sub Type], [Sub Type Description], case when [Eway Bill Transaction Type] = 'R' then 'Regular' else [Eway Bill Transaction Type] end [Eway Bill Transaction Type], [Customer Billing GSTIN], [Customer Billing Name], [Customer Billing Address], [Customer Billing Address 2], [Customer Billing City], CONVERT(INT, [Customer Billing Pincode]) [Customer Billing Pincode], [Customer Billing State], [Supplier GSTIN], [Supplier Name], [Supplier Address1], [Supplier Address2], [Supplier Place], CONVERT(INT, [Supplier Pincode]) [Supplier Pincode], [Supplier State], [Customer Shipping Name], [Customer Shipping Address], [Customer Shipping Address 2], [Customer Shipping City], CONVERT(INT, [Customer Shipping Pincode]) [Customer Shipping Pincode], [Customer shipping State], [Transporter ID], [Transporter Name], [Transportation Mode (Road/Rail/Air/Ship)], [Distance Level (Km)], [Transporter Doc No], REPLACE([Transportation Date], '-', '/'), [Vehicle Number], [Vehicle Type] from [dbo].[EWAY_BILL_FORMAT_FIDR1376_FOR_EWAY_PURPOSE] where [ACTIVE] = '0'"         
             cur.execute(Header_query)
             rows = cur.fetchall()
             cur.close()
+            connection.close()
             servicelogger_info.info("...Data fetch from [EWAY_BILL_FORMAT_FIDR1376_FOR_EWAY_PURPOSE] table from database for generate E-Way Bill...\n")
+            connection.close()
             return rows
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured in executing the Header Query from database for generate EWB :\n ")
+        finally:
+            connection.close()
 
     def executeLineDBQuery(document_No):
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             Lineitem_query = 'select [Product Name], [Item Description], [HSN code], CONVERT(DECIMAL(8,2),[Item Quantity]) [Item Quantity] , [Item Unit of Measurement], [Taxable Value], [CGST RATE], [cgst amount], [SGST RATE], [Sgst amount], [IGST RATE], [Igst amount], [CESS Rate], [CESS Amount], [Other Value], [CESS Non Advol Tax Amount] from [dbo].[EWAY_BILL_FORMAT_FIDR1376_FOR_EWAY_PURPOSE] where [Document Number] = \'{}\''.format(document_No)
             cur.execute(Lineitem_query)
             rows = cur.fetchall()
             cur.close()
+            connection.close()
             return rows
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured in executing the Line Item Query from database for generate EWB :\n ")
+        finally:
+            connection.close()
 
     def persistSuccessResponseInDB(Status,EwbNo,EwbDt,EwbValidTill,DocumentNumber, payload, response_data):
         try:
+            connection = database.databaseConnection()
             json_Reqpayload = json.dumps(payload, default=decimal_default)
             json_ResponsePayload = json.dumps(response_data, default=decimal_default)
             cur_2 = connection.cursor()
@@ -89,13 +111,17 @@ class database:
             connection.commit()
 
             cur_2.close()
+            connection.close()
             servicelogger_info.info("... Response Records from ClearTax API for generate EWB inserted into [EWB_NON_IRN_RESPONSE] table for Success status...\n")
         except Exception as e:
             print("Error in Insert Success EWB Generation in db : ",e)
             servicelogger_error.exception("Exception Occured for Success response in database for generate EWB :\n ")
+        finally:
+            connection.close()
 
     def persistFailureResponseInDB(fail_status,error_message, error_code, error_source, DocumentNumber, payload, response_data):
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             json_requestPayload = json.dumps(payload ,default=decimal_default)
             json_responsePayload = json.dumps(response_data ,default=decimal_default)
@@ -111,27 +137,35 @@ class database:
             connection.commit()
 
             cur.close() 
+            connection.close()
             servicelogger_info.info("... Response Records from ClearTax API for generate EWB inserted into [EWB_NON_IRN_RESPONSE] table for Failure status...\n")
         except Exception as e:
             print("Error in Insert Fail EWB Generation in db : ",e)
             servicelogger_error.exception("Exception Occured for Failure response in database for generate EWB :\n ")
+        finally:
+            connection.close()
 
     # Cancel E-way Bill
     def executeCancelEWBHeaderQuery():
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             Header_query = "SELECT [EWAY_BILL_NO], [USER_GSTIN], [CANCELLATION_REASON], [REASON_REMARKS] FROM [dbo].[ICUST_IIL096_C] WHERE [ACTIVE] = '0'"
             cur.execute(Header_query)
             rows = cur.fetchall()
             cur.close()
             servicelogger_info.info("...Data fetch from [ICUST_IIL096_C] table from database for cancel E-Way Bill...\n")
+            connection.close()
             return rows
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured in executing the database Query for cancel EWB :\n ")
-    
+        finally:
+            connection.close()
+
     def persistCancelEWBSuccessResponseInDB(gstin,irn,ewbNumber,ewbStatus, cancelEWBpayload, response_data):
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             json_requestPayload = json.dumps(cancelEWBpayload ,default=decimal_default)
             json_responsePayload = json.dumps(response_data ,default=decimal_default)
@@ -147,13 +181,17 @@ class database:
             connection.commit()
 
             servicelogger_info.info("... Response Records from ClearTax API for Cancel EWB inserted into [CANCEL_RESPONSE_DATA] table for Success status...\n")
-            cur.close() 
+            cur.close()
+            connection.close() 
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured for Success response in database for cancel EWB :\n ")
+        finally:
+            connection.close()
 
     def persistCancelEWBFailureResponseInDB(ewbStatus,error_message, cancelEWBpayload, response_data, ewbNo):
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             json_requestPayload = json.dumps(cancelEWBpayload ,default=decimal_default)
             json_responsePayload = json.dumps(response_data ,default=decimal_default)
@@ -170,26 +208,34 @@ class database:
             connection.commit()
 
             cur.close()
+            connection.close()
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured for Failure response in database for cancel EWB :\n ")
-    
+        finally:
+            connection.close()
+
     # Update E-Way Bill
     def executeUpdateEWBHeaderQuery():
         try:
+            connection = database.databaseConnection()
             cur = connection.cursor()
             Header_query = "SELECT [EWAY_BILL_NO], [USER_GSTIN], [FROM_PLACE], [From_State],[Reason_Code], [REASON_REMARKS], [Transport_Document_No], [Transport_Document_Date], [Transport_Mode], [Document_No], [DOCUMENT_TYPE], [Document_Date], [Vehicle_Type], [Vehicle_No] FROM [dbo].[ICUST_IIL097_C] WHERE [ACTIVE] = '0'"
             cur.execute(Header_query)
             rows = cur.fetchall()
             cur.close()
             servicelogger_info.info("...Data fetch successfully from [ICUST_IIL097_C] table from database for update E-Way Bill...\n")
+            connection.close()
             return rows
         except Exception as e:
             print("Error : ",e)
             servicelogger_error.exception("Exception Occured in executing the database Query for update EWB :\n ")
-    
+        finally:
+            connection.close()
+
     def persistUpdateEWBSuccessResponseInDB(EWB_No, Updated_date, valid_upto, updateEWBpayload, response_data):
         try:
+            connection = database.databaseConnection()
             json_requestPayload = json.dumps(updateEWBpayload ,default=decimal_default)
             json_responsePayload = json.dumps(response_data ,default=decimal_default)
             cur = connection.cursor()
@@ -206,12 +252,16 @@ class database:
             connection.commit()
             servicelogger_info.info("... Response Records from ClearTax API for Update EWB inserted into [UPDATE_RESPONSE_DATA] table for Success status...\n")
             cur.close() 
+            connection.close()
         except Exception as e:
             print("Error in Success update for EWB Update : ",e)
             servicelogger_error.exception("Exception Occured for Success response in database for update EWB :\n ")
+        finally:
+            connection.close()
 
     def persistUpdateEWBFailureResponseInDB(error_message, updateEWBpayload, response_data, ewbNo):
         try:
+            connection = database.databaseConnection()
             json_requestPayload = json.dumps(updateEWBpayload ,default=decimal_default)
             json_responsePayload = json.dumps(response_data ,default=decimal_default)
             cur = connection.cursor()
@@ -228,8 +278,10 @@ class database:
             connection.commit()
             
             cur.close()
+            connection.close()
         except Exception as e:
             print("Error in failure update for EWB Update: ",e)
             servicelogger_error.exception("Exception Occured for Failure response in database for update EWB :\n ")
-
+        finally:
+            connection.close()
    
